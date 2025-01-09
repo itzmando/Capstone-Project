da8f0a67cf855eca07813db89744ab84a3ea73bd
Daniel Boachie <dboachie222@gmail.com>
2025-01-08 22:40:53 -0500

    Seeded Database

--- db/index.js
+++ db/index.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 const db = new Pool({
     connectionString:
         process.env.DATABASE_URL ||
-        "postgres://localhost:5432/acme_world_travel_review_db",
+        "postgres://dkb@localhost:5432/acme_world_travel_review_db",
 });
 
-async function query(sql, params, callback) {
-    return db.query(sql, params, callback);
-}
+// async function query(sql, params, callback) {
+//     return db.query(sql, params, callback);
+// }
 
-module.exports = { query, db };
+module.exports = { db };
--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,6 @@
 // Clear and repopulate the database.
 
-const db = require("./index");
+const { db } = require("./index");
 const { faker } = require("@faker-js/faker");
 
 async function seed() {
@@ -24,7 +24,7 @@ async function seed() {
         profile_picture_url VARCHAR(255) NOT NULL,
         bio TEXT NOT NULL,
         country VARCHAR(100) NOT NULL,
-        created_at TIMESTAMP NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
         last_login TIMESTAMP
 
 
@@ -85,7 +85,7 @@ async function seed() {
     CREATE TABLE bookmarks (
         user_id SERIAL REFERENCES users(id) NOT NULL,
         place_id SERIAL REFERENCES places(id) NOT NULL,
-        created_at TIMESTAMP NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
         notes TEXT NOT NULL,
         PRIMARY KEY (user_id, place_id)
 
@@ -95,8 +95,8 @@ async function seed() {
     CREATE TABLE operating (
       place_id SERIAL REFERENCES places(id) NOT NULL,
       day_of_week INT NOT NULL,
-      open_time TIME NOT NULL,
-      close_time TIME NOT NULL,
+      open_time TIMESTAMP NOT NULL,
+      close_time TIMESTAMP NOT NULL,
       is_closed BOOLEAN NOT NULL,
       PRIMARY KEY (place_id, day_of_week)
 
@@ -112,8 +112,8 @@ async function seed() {
         title VARCHAR(200) NOT NULL,
         content TEXT NOT NULL,
         visit_date DATE NOT NULL,
-        created_at TIME NOT NULL,
-        updated_at TIME NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMP NOT NULL,
         comments VARCHAR(255) NOT NULL
 
       );
@@ -144,8 +144,106 @@ async function seed() {
 
       );
 
+      
+
 
     `);
+
+
+    let user_id, category_id, country_id, city_id, latitude, longitude, population, place_id, day_of_week, review_id, rating, photo_id, price_level = 0;
+    let is_featured = true;
+    let is_helpful = true;
+    let is_closed = true;
+    let username, email, password_hash, full_name, profile_picture_url, bio, country, name, description, icon_url, continent,
+      state_province, timezone, address, website_url, phone_number, notes, open_time, close_time, title,
+      content, visit_date, comments, updated_at, photo_url, caption, uploaded_at, commented_at = ""
+
+    const createData = async (numOfSeeds) => {
+
+      for (let i = 0; i < numOfSeeds; i++) {
+        user_id = i + 1, username = `user${i}`, email = `userEmail${i}`, password_hash = `passwordHash${i}`, full_name = `fullName${i}`,
+
+          profile_picture_url = `profile_picture_url${i}`, bio = `bio${i}`, country = `country${i}`,
+
+          category_id = i + 1, name = `name${i}`, description = `description${i}`, icon_url = `iconUrl${i}`,
+
+          country_id = i + 1, name = `name${i}`, continent = `continent${i}`,
+
+          city_id = i + 1, name = `name${i}`, state_province = `state_province${i}`, latitude = i, longitude = i, timezone = `timezone${i}`, population = i,
+
+          place_id = i + 1, name = `name${i}`, description = `description${i}`, address = `address${i}`, latitude = i, longitude = i, website_url = `websiteUrl${i}`,
+
+          phone_number = `phoneNumber${i}`, price_level = i, notes = `notes${i}`, day_of_week = i,
+
+          open_time = `2025-01-08  00:${i + 10}:00`, close_time = `2025-01-08  00:${i + 10}:00`, review_id = i + 1, rating = i,
+
+          title = `title${i}`, content = `content${i}`, visit_date = `2025-01-${i + 10}`,
+
+          updated_at = `2025-01-08  00:${i + 10}:00`, comments = `comments${i}`, photo_id = i + 1, photo_url = `photo_url${i}`, caption = `caption${i}`,
+
+          uploaded_at = `2025-01-08  00:${i + 10}:00`, commented_at = `2025-01-08  00:${i + 10}:00`;
+
+
+        await db.query(`
+          INSERT INTO users(user_id, username, email, password_hash, full_name, profile_picture_url, bio, country)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        ;`, [user_id, username, email, password_hash, full_name, profile_picture_url, bio, country]);
+
+        await db.query(`
+          INSERT INTO categories(category_id, name, description, icon_url)
+          VALUES ($1, $2, $3, $4)
+          ;`, [category_id, name, description, icon_url]);
+
+        await db.query(`
+            INSERT INTO countries(country_id, name, continent)
+            VALUES ($1, $2, $3)
+            ;`, [country_id, name, continent]);
+
+        await db.query(`
+              INSERT INTO cities(city_id, country_id, name, state_province, latitude, longitude, timezone, population)
+              VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+              ;`, [city_id, country_id, name, state_province, latitude, longitude, timezone, population]);
+
+
+        await db.query(`
+              INSERT INTO places(place_id, name, description, address, latitude, longitude, website_url, phone_number, price_level)
+              VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+              ;`, [place_id, name, description, address, latitude, longitude, website_url, phone_number, price_level]);
+
+        await db.query(`
+              INSERT INTO bookmarks(notes)
+              VALUES ($1)
+              ;`, [notes]);
+
+        await db.query(`
+              INSERT INTO operating(day_of_week, open_time, close_time, is_closed)
+              VALUES ($1, $2, $3, $4)
+                ;`, [day_of_week, open_time, close_time, is_closed]);
+
+        await db.query(`
+              INSERT INTO reviews(review_id, rating, title, content, visit_date, updated_at, comments)
+              VALUES ($1, $2, $3, $4, $5, $6, $7)
+                ;`, [review_id, rating, title, content, visit_date, updated_at, comments]);
+
+        await db.query(`
+              INSERT INTO photos(photo_id, photo_url, caption, uploaded_at, is_featured)
+              VALUES ($1, $2, $3, $4, $5)
+                ;`, [photo_id, photo_url, caption, uploaded_at, is_featured]);
+
+        await db.query(`
+              INSERT INTO comments(is_helpful, commented_at)
+              VALUES ($1, $2)
+                ;`, [is_helpful, commented_at]);
+
+
+      }
+
+      // await createData(numOfSeeds);
+      // console.log("Seeded")
+
+    }
+    await createData(10);
+    console.log("Seeded")
 
 
   } catch (err) {
